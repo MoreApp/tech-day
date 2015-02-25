c600b38ef2483ed6b56980c55cfa9560f8a7a3b9
Mats Stijlaart <m.stijlaart@avisi.nl>
2015-02-25 15:56:54 +0100

    Fix for config and response

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
-{
+module.exports = {
   "tunnel" : null,
   "plugin" : {
     "namespace" : null, // Example: "nl.avisi.matstijl"
     "key" : null, //Example: "matstijl"
     "name" : null //Example: "Mats is Awesome"
   }
-}
+};
--- index.js
+++ index.js
@@ -7,15 +7,11 @@ app.use(bodyParser.json());
 
 app.post('/handle', function (req, res) {
   var body = req.body;
-
   console.log("Form Name:", body.view.meta.name);
-  console.log("Registration Id:", body.registration);
+  console.log("Registration Id:", body.registration.data);
   console.log("Configuration", body.configuration);
 
-
-  console.log(req.body);
-  console.log(req.body);
-  res.send("Hello world");
+  res.json(null);
 });
 
 app.use(express.static(path.join(__dirname, 'public')));
